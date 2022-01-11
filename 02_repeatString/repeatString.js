fcad7712c98df0e4444379a7d776b909f7ebe4b8
dev-yoga <spin_shoppe@icloud.com>
2022-01-11 11:56:53 -0800

    almost 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+//this function will take in two arguments, string and num
+const repeatString = function(word, num) {
+// create a variable to hold the string you're going to return
+    let onRepeat = '';
+// create a loop that repeats the given number of times
+    for (let i = 0; i < num; i++) {
+// add the given string to the result on each loop
+    onRepeat += word;
+    }
+    return onRepeat;
 };
 
 // Do not edit below this line
