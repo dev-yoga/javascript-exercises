9539ca824eac0e5953493bc7f836de0b6cd5322e
dev-yoga <spin_shoppe@icloud.com>
2022-01-12 16:32:50 -0800

    reverseString refactor

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,19 @@
+// first iterative pass (passes tests, checked at each step):
+
+// const reverseString = function(str) {
+//     const splitString = str.split('');
+//     const reverseSplit = splitString.reverse();
+//     return reverseSplit.join('');
+// };
+
+// refactored (and passes tests)
 const reverseString = function(str) {
-    const splitString = str.split('');
-    const reverseSplit = splitString.reverse();
-    return reverseSplit.join('');
+    return str
+    .split('')
+    .reverse()
+    .join('');
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
