c8fb5d758e52b2c43a5a7e0d0d9a4ff34f6b9707
dev-yoga <spin_shoppe@icloud.com>
2022-01-14 14:01:14 -0800

    04 Passes first test but not subsequent tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+
+    // console.log(arr);
+    // console.log(x);
+    // const id = arr.indexOf(x);
+    // console.log(id);
+    // return arr.splice(id, 1);
+
+    // passes first test but ;;
+    
+    return arr.filter(function(ele){
+        return ele !== value;
+    });
 
 };
 
