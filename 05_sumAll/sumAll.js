979ac0d7ffbf28e197d9aef270b0228cae43be54
dev-yoga <spin_shoppe@icloud.com>
2022-01-28 20:18:36 -0800

    Passing more tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,12 @@
 const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    if (num1 > num2) {
+        const flip = num1;
+        num1 = num2;
+        num2 = flip;
+    }
     let sum = 0;
     for (let i = num1; i < num2 + 1; i++) {
         sum += i;
