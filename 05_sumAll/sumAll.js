b0ff1e17c9a70ad492b1447b5602f8dddbd2c01d
dev-yoga <spin_shoppe@icloud.com>
2022-01-28 20:24:24 -0800

    Number is integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,9 @@ const sumAll = function(num1, num2) {
     if (num1 < 0 || num2 < 0){
         return 'ERROR';
     }
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
     if (num1 > num2) {
         const flip = num1;
         num1 = num2;
