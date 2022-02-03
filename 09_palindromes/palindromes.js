48dc03c936fd96ec37b74344fd84d18a4da8b224
dev-yoga <spin_shoppe@icloud.com>
2022-02-03 14:56:10 -0800

    Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,19 @@
-const palindromes = function(palindrome) {
-    let split = palindrome.split('');
-    let reverse = split.reverse();
-    let join = reverse.join('');
-    if (join === palindrome) {
-        return true;
-    } else {
-        return false;
-    }
-
+const palindromes = function(string) {
+    // let split = palindrome.split('');
+    // let reverse = split.reverse();
+    // let join = reverse.join('');
+    // if (join === palindrome) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    maybePalindrome = string.toLowerCase().replace(/[^a-z]/g, '');
+    return (
+        maybePalindrome
+        .split('')
+        .reverse()
+        .join('') === maybePalindrome
+    );
 };
 
 // Do not edit below this line
