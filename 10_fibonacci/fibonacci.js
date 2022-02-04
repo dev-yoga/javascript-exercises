e55ed18829b45dab7e81e6c07e82cf371e6671a4
dev-yoga <spin_shoppe@icloud.com>
2022-02-03 17:52:31 -0800

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(selection) {
+    if (selection < 0){
+        return 'OOPS'
+    };
+    if (selection === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < selection; i++) {
+        const num1 = b;
+        b = a + b;
+        a = num1;
+    }
+    return b;
 };
 
 // Do not edit below this line
